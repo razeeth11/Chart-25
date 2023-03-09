ffb96bf81714bf48b92003550685659dd23edd5c
razeeth11 <razeeth011@gmail.com>
2023-03-09 18:53:19 +0530

    Chart-js

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ForumIcon from '@mui/icons-material/Forum';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line, Doughnut } from 'react-chartjs-2';
+import faker from 'faker';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+  );
+  
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Earnings Overview',
+    },
+  },
+};
+
+const labels = ['January', 'March',  'May', 'June', 'July' , 'September'  ,'November' ];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Earnings ',
+      data: labels.map(() => faker.datatype.number({ min: 10000, max: 40000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function App(){
+
+  return(
+    <div>
+      <Cards/>
+      <div className='Main'>
+      <Chart/>
+      <RoundChart/>
+      </div>
+    </div>
+  )
+}
+
+export function Chart() {
+  return(
+    <div className='App'> 
+      <Line options={options} data={data} /> 
+    </div>
+  ) 
+}
+
+function Cards(){
+  const data = [
+    {
+      earnings : "EARNINGS(MONTHLY)",
+      amount : "$40000",
+      icon :  <CalendarTodayIcon/>
+    },
+    {
+      earnings : "EARNINGS(ANNUAL)",
+      amount : "$215,000",
+      icon : <AttachMoneyIcon/>
+    },
+    {
+      earnings : "TASKS",
+      amount : "50%",
+      icon : <CalendarMonthIcon/>
+    },
+    {
+      earnings : "PENDING REQUESTS",
+      amount : "18",
+      icon : <ForumIcon/>
+    },
+  ]
+  return(
+    <div className='cards'>
+         {data.map((mv)=> <Card earnings={mv.earnings} amount={mv.amount} icon={mv.icon}/>)}
+    </div>
+  )
+}
+
+export function Card({earnings,amount,icon}){
+  return(
+          <div className='Card'>
+      <div>
+      <p><strong>{earnings}</strong></p>
+      <h3>{amount}</h3>
+      </div>
+      <div>
+        {icon}
+      </div>
+          </div>
+  )
+}
+
+
+
